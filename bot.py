bf33e35e28d3f6061e4ce5406697e437a14552df
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-11-12 20:05:23 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,6 @@ import sys
 from datetime import datetime
 from config import API_HASH, API_ID, LOGGER, BOT_TOKEN, TG_BOT_WORKERS, CHANNEL_ID, PORT
 import pyrogram.utils
-from plugins.fsub import FORCE_SUB_CHANNEL
 
 pyrogram.utils.MIN_CHANNEL_ID = -1009999999999
 
