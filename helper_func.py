9f9a2a306793a1e20c75ba3db3fb41cd4c1b9e59
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-11-12 19:56:02 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -3,7 +3,8 @@ import re
 import asyncio
 from pyrogram import filters
 from pyrogram.enums import ChatMemberStatus
-from config import FORCE_SUB_CHANNEL, ADMINS
+from config import ADMINS
+from plugins.fsub import FORCE_SUB_CHANNEL
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 from pyrogram.errors import FloodWait
 
